c515ea556d2615741acab78e563d44b7a7bd7fe3
johnmrtn <johnmartin.pabriaga@students.isatu.edu.ph>
2024-09-27 10:13:45 +0800

    second commit

---
+++ RWorksheet_Pabriaga#2.R
@@ -0,0 +1,135 @@
+---
+title: "RWorksheet_Pabriaga#2"
+author: "John Martin S. Pabriaga"
+date: "2024-09-26"
+output: html_document
+---
+# 1.a
+seq_vec <- -5:5
+seq_vec
+
+# 1.b
+x <- 1:7
+x
+
+# 2.
+seq_vec <- seq(1, 3, by = 0.2)
+seq_vec
+
+# 3. 
+ages <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 
+          57, 49, 50, 37, 46, 25, 17, 37, 43, 53, 41, 51, 35, 24, 33, 41, 53, 40, 
+          18, 44, 38, 41, 48, 27, 39, 19, 30, 61, 54, 58, 26, 18)
+
+# 3.a
+ages[3]
+
+# 3.b
+ages[c(2, 4)]
+
+# 3.c
+ages[-c(4, 12)]
+
+# 4.
+x <- c("first" = 3, "second" = 0, "third" = 9)
+x
+
+# 4.a
+x[c("first", "third")]
+
+# 5.
+x <- -3:2
+x[2] <- 0
+x
+
+# 6. 
+month <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun")
+price_per_liter <- c(52.50, 57.25, 60.00, 65.00, 74.25, 54.00)
+purchase_quantity <- c(25, 30, 40, 50, 10, 45)
+
+# 6.a
+fuel_data <- data.frame(
+  Month = month,
+  Price_per_Liter = price_per_liter,
+  Purchase_Quantity = purchase_quantity
+)
+fuel_data
+
+# 6.b
+avg_expenditure <- weighted.mean(price_per_liter, purchase_quantity)
+avg_expenditure
+
+# 7. 
+data(rivers)
+data <- c(length(rivers), sum(rivers), mean(rivers), median(rivers), var(rivers), sd(rivers), min(rivers), max(rivers))
+data
+
+# 8.a
+power_ranking <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25)
+celebrity_name <- c("Tom Cruise", "Rolling Stones", "Oprah Winfrey", "U2", 
+                    "Tiger Woods", "Steven Spielberg", "Howard Stern", "50 Cent", 
+                    "Cast of the Sopranos", "Dan Brown", "Bruce Springsteen", 
+                    "Donald Trump", "Muhammad Ali", "Paul McCartney", "George Lucas", 
+                    "Elton John", "David Letterman", "Phil Mickelson", "J.K. Rowling", 
+                    "Brad Pitt", "Peter Jackson", "Dr. Phil McGraw", "Jay Leno", 
+                    "Celine Dion", "Kobe Bryant")
+pay <- c(67, 90, 225, 110, 90, 332, 302, 41, 52, 88, 55, 44, 55, 40, 233, 34, 17, 47, 75, 20, 39, 45, 32, 40, 31)
+forbes_data <- data.frame(PowerRanking = power_ranking, 
+                          CelebrityName = celebrity_name, 
+                          Pay = pay)
+print(forbes_data)
+
+# 8.b
+forbes_data[forbes_data$CelebrityName == "J.K. Rowling", "PowerRanking"] <- 15
+forbes_data[forbes_data$CelebrityName == "J.K. Rowling", "Pay"] <- 90
+print(forbes_data)
+
+# 8.c
+write.csv(forbes_data, file = "PowerRanking.csv", row.names = FALSE)
+imported_data <- read.csv("PowerRanking.csv")
+print(imported_data)
+
+# 8.d
+subset_data <- forbes_data[10:20, ]
+save(subset_data, file = "Ranks.RData")
+print(subset_data)
+
+# 9.a
+file_path <- "/Users/johnmartin/Desktop/CS101/hotels-vienna.xlsx" 
+hotels_data <- read_excel(file_path)
+hotels_data
+
+# 9.b
+dimensions <- dim(hotels_data)
+dimensions
+
+# 9.c 
+selected_columns <- hotels_data[, c("country", "neighbourhood", "price", "stars", "accommodation_type", "rating")]
+print(head(selected_columns))
+
+# 9.d
+save(selected_columns, file = "new.RData")
+dir()
+# 9.e 
+load("~/Desktop/CS101/new.RData")
+print(head(selected_columns))
+print(tail(selected_columns))
+
+# 10.a 
+vegetables <- list("Squash", "Carrot", "String Beans", "Moringa leaves", "Cabbage", 
+                   "Bitter Gourd", "Potato", "Eggplant", "Lettuce", "Water spinach")
+print(vegetables)
+
+# 10.b
+vegetables <- append(vegetables, c("Cauliflower", "Peas"))
+print(vegetables)
+
+# 10.c
+vegetables <- append(vegetables, c("Spinach", "Broccoli", "Mushroom", "Bell Pepper"), after = 5)
+print(vegetables)
+length(vegetables)
+
+# 10.d
+vegetables <- vegetables[-c(5, 10, 15)]
+print(vegetables)
+length(vegetables)
